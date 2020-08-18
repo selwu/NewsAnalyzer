68e2542b3211b8ca0b203ab3a27104a13822b254
selwu <selwu@mail.ru>
2020-08-18 20:11:59 +0300

    done project, bug fix

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,7 +2,7 @@ const DATE_FROM_DAYS_AGO = 6;
 const ERROR_MESSAGE_INPUT = 'Поле не должно быть пустым';
 // https://praktikum.tk/news  https://newsapi.org  https://nomoreparties.co/news
 
-const BASE_URL_NEWS_API = 'https://newsapi.org';
+const BASE_URL_NEWS_API = 'https://nomoreparties.co/news';
 const KEY_NEWS_API = 'b9e026f62a0d460d9e07404a0e48c4a7';
 
 export { DATE_FROM_DAYS_AGO, ERROR_MESSAGE_INPUT, BASE_URL_NEWS_API, KEY_NEWS_API };
